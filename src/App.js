ba16fa51ddee4b07485711ac4bf8a861453d1c70
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-08-29 14:15:33 -0400

    - setup components - navbar done - scss done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
+import { match } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Navigation />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 registerServiceWorker();
