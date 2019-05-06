13d3e6a041132b1e0062ce8b912e0c8180c63c8b
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-05-06 16:12:34 -0400

    add padding bottom to show pagination

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -64,7 +64,7 @@ export default class Filter extends React.Component {
             <label>
               Cover Letter<br />
               <input type="checkbox"></input>
-              <span class="lever"></span>
+              <span className="lever"></span>
               Required
             </label>
           </div>
