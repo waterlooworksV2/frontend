1ef894829ac6b55729a0ae5da36c6c8bd5af760f
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-05-08 13:01:02 -0400

    disable hide on scroll

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -93,7 +93,7 @@ export default class Home extends Component {
     if(prevScrollpos > currentScrollPos){
       visible = '';
     } else{
-      visible = 'hide';
+      visible = '';
     }
 
     this.setState({
