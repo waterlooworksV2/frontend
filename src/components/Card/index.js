5be64e2efe88715cf52c6f74a1560619c1c86076
subrasub <s35subra@edu.uwaterloo.ca>
2019-05-17 00:27:36 -0400

    Cleanup

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -26,30 +26,36 @@ export default class Card extends React.Component {
     return JobService.getPreviewJobID(id)
   }
 
+  huegenerator = () => {
+    return 'rgb(' + (Math.floor((205)*Math.random())+50) + ',' + (Math.floor((205)*Math.random())+50) + ',' + (Math.floor((205)*Math.random())+50) + ')';
+  }
+
   componentDidMount(){
       $('.card').each(function () {
-          var hue = 'rgb(' + (Math.floor((205)*Math.random())+50) + ',' + (Math.floor((205)*Math.random())+50) + ',' + (Math.floor((205)*Math.random())+50) + ')';
-          $(this).css("border-top-color", hue);
+          $(this).css("border-top-color", this.huegenerator());
       });
       if(this.props.render !== false) {
         this.getJob(this.props.id).then(data => {
-          this.setState({ job: data });
-          this.setState({ blur: '' });
+          this.setState({ 
+            job: data, 
+            blur: '' 
+          });
         });
       }
   }
 
   render() {
-    return (<a href={'#'+this.props.id} className="jobCard" onClick={() => this.props.onClickCard(this.props.id)}>
-              <div className="hover-up card" style= { {"borderTop": "4px solid", "WebkitUserSelect": "none", "MozUserSelect": "none", "msUserSelect": "none", "userSelect": "none"} }>
-                <div className="box-shadow">
-                  <div className="project-box flow-text">
-                    <p className={`primary ${this.state.blur}`}>{this.state.job["Job Title:"]}, {this.state.job["Organization:"]}</p>
-                    <p className={`secondary ${this.state.blur}`}>{this.state.job["Job Summary:"]}</p>
-                  </div>
-                </div>
-              </div>
-            </a>
+    return (
+      <a href={'#'+this.props.id} className="jobCard" onClick={() => this.props.onClickCard(this.props.id)}>
+        <div className="hover-up card" style= { {"borderTop": "4px solid", "WebkitUserSelect": "none", "MozUserSelect": "none", "msUserSelect": "none", "userSelect": "none"} }>
+          <div className="box-shadow">
+            <div className="project-box flow-text">
+              <p className={`primary ${this.state.blur}`}>{this.state.job["Job Title:"]}, {this.state.job["Organization:"]}</p>
+              <p className={`secondary ${this.state.blur}`}>{this.state.job["Job Summary:"]}</p>
+            </div>
+          </div>
+        </div>
+      </a>
     );
   }
 }
